9c84495a6f6a22330c4e7dced63eef457ef60c4c
pyminsk <pyminsk@yahoo.com>
2017-07-25 17:52:11 +0200

    first

---
+++ age.py
@@ -0,0 +1,6 @@
+import sys
+def silly_age_joke(age):
+    if age >= 10 and age <= 13:
+        print('13 + 49 + 84 + 155 + 97: что получится? Головная боль!')
+    else:
+        print('Что-что?')
---
+++ turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+t = turtle.Turtle()
+t.pensize(3)
+t.speed(0)
+
+t.pencolor("black")
+for i in range(119):
+    t.forward(20)
+    t.left(137)
+	
+turtle.done()
